47daa7ee77cab96aeec08b3308b955a23c6a8ba0
Insolor <Insolor@localhost>
2013-09-19 14:56:18 +0400

    Added drive letter to build script

--- changetext.bat
+++ changetext.bat
@@ -1,3 +1,3 @@
 @echo off
-gcc -c -DBUILD_DLL changetext.c -I/Python33/include
-gcc -shared -o changetext.dll -Wl,--out-implib,libchangetext.a changetext.o -L/Python33/libs -lpython33
+gcc -c -DBUILD_DLL changetext.c -IC:/Python33/include
+gcc -shared -o changetext.dll -Wl,--out-implib,libchangetext.a changetext.o -LC:/Python33/libs -lpython33
