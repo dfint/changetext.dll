af9b311e6e570ca581fbb487a502ec2ad5b86f50
Mike Manturov <insolor@gmail.com>
2013-10-11 12:36:51 +0400

    Added -O2 compilation key. Nuff said.

--- compile_changetext.bat
+++ compile_changetext.bat
@@ -1,3 +1,3 @@
 @echo off
-gcc -c -DBUILD_DLL changetext.c -IC:/Python33/include
+gcc -c -DBUILD_DLL -O2 changetext.c -IC:/Python33/include
 gcc -shared -o changetext.dll -Wl,--out-implib,libchangetext.a changetext.o -LC:/Python33/libs -lpython33
