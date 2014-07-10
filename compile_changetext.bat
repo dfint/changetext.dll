577007ef58e7f2bf62cf99d05b504e6ef31e2e65
Insolor <Insolor@localhost>
2014-07-10 22:39:13 +0400

    Migration to the Python34

--- compile_changetext.bat
+++ compile_changetext.bat
@@ -1,3 +1,3 @@
 @echo off
-gcc -c -DBUILD_DLL -O2 changetext.c -IC:/Python33/include
-gcc -shared -o changetext.dll -Wl,--out-implib,libchangetext.a changetext.o -LC:/Python33/libs -lpython33
+gcc -c -DBUILD_DLL -O2 changetext.c -IC:/Python34/include
+gcc -shared -o changetext.dll -Wl,--out-implib,libchangetext.a changetext.o -LC:/Python34/libs -lpython34
