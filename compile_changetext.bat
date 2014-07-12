9e13c307c7e023abe5af855b3ca92120407b6b67
Insolor <Insolor@localhost>
2014-07-12 18:16:28 +0400

    Backed out changeset: e8094d012b46

--- compile_changetext.bat
+++ compile_changetext.bat
@@ -1,3 +1,3 @@
 @echo off
-g++ -c -DBUILD_DLL -O2 changetext.c -IC:/Python34/include
-g++ -shared -o changetext.dll -Wl,--out-implib,libchangetext.a changetext.o -LC:/Python34/libs -lpython34
+gcc -c -DBUILD_DLL -O2 changetext.c -IC:/Python34/include
+gcc -shared -o changetext.dll -Wl,--out-implib,libchangetext.a changetext.o -LC:/Python34/libs -lpython34
