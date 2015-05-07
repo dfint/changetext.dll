56a62bf0dfc2fd0b130228bce35ac04d0900ec45
Insolor <Insolor@localhost>
2015-05-07 18:56:43 +0300

    Force changetext.py to work with raw bytes

--- changetext.py
+++ changetext.py
@@ -1,8 +1,18 @@
 
+
 def ChangeText(s):
-    if s == 'hello':
-        return 'HELLO'
+    print('ChangeText() received:', s)
+    decoded = s.decode("utf-16")
+    print('Decoded:', decoded)
+    
+    if decoded == 'hello':
+        returned = 'world'
+    
+    b = returned.encode("utf-16")[2:]+b'\0\0'
+    
+    return b
+
 
 if __name__ == '__main__':
-    print(ChangeText('hello'))
+    print(ChangeText(b'h\0e\0l\0l\0o\0'))
     input()
